01d4a3ebc72789b3aa24de8d920b356a24dad41f
Evandro oliveira <vandoaparecido@hotmail.com>
2020-08-17 16:27:08 -0300

    Instalação do knex e sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.post('/users', (request, response) =>{
+    const users = [
+        {nome:'Evandro', age: 36},
+        {nome:'Israel', age: 12},
+        {nome:'Carol', age: 32}
+    ];
+    
+    return response.send(users);
+});
+
 app.listen(3333);
