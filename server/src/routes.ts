49712b84392668ad44144744fa56e55c302c4d2d
Evandro oliveira <vandoaparecido@hotmail.com>
2020-08-20 07:57:19 -0300

    Tratamento de erros

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -24,34 +24,44 @@ routes.post('/classes', async (request, response) =>{
 
     const trx = await db.transaction();
     
-const insertedUsersId = await trx('users').insert({
-    name, 
-    avatar,
-    watsapp,
-    bio,
-});
+    try{
 
-const user_id = insertedUsersId[0];
+        const insertedUsersId = await trx('users').insert({
+            name, 
+            avatar,
+            watsapp,
+            bio,
+        });
+    
+        const user_id = insertedUsersId[0];
+    
+        const insertedClassesId = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        })
+    
+        const class_id = insertedClassesId[0];
+        const classSchedule = schedule.map((scheduleItem: Scheduleitem) =>{
+            return{
+                class_id,
+                week_day:scheduleItem.week_day,
+                from: convertHourToMinuts(scheduleItem.from),
+                to: convertHourToMinuts(scheduleItem.to),
+            };
+        })
+    
+        await trx('classes_schedule').insert(classSchedule);
+            trx.commit();   
+            return response.status(2001).send();
 
-const insertedClassesId = await trx('classes').insert({
-    subject,
-    cost,
-    user_id,
-})
+    }catch(err){
+        await trx.rollback();
+        return response.status(400).json({
+            error:'Unexpected error while creating nrw class'
+        })
+    }
 
-const class_id = insertedClassesId[0];
-const classSchedule = schedule.map((scheduleItem: Scheduleitem) =>{
-    return{
-        class_id,
-        week_day:scheduleItem.week_day,
-        from: convertHourToMinuts(scheduleItem.from),
-        to: convertHourToMinuts(scheduleItem.to),
-    };
-})
-
-await trx('classes_schedule').insert(classSchedule);
-    trx.commit();   
-    return response.send();
 });
 
 export default routes;
