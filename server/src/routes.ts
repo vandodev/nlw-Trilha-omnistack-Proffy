e222f8cf58c0535e2b8f929425cf3c6e2d7e27e2
Evandro oliveira <vandoaparecido@hotmail.com>
2020-08-21 23:14:19 -0300

    Criando connection

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import ClassesControllers from './controllers/ClassesControllers';
+import ConnectionsController from './controllers/ConnectionsController';
+
 
 
 const routes = express.Router();
 const classesController = new ClassesControllers();
+const connectionsController = new ConnectionsController();
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
+
+routes.post('/connections', connectionsController.create);
 
 export default routes;
